69803a6e0f13c316f9b0b07fddfc84a72ff1b42e
Vojtech Faltynek <faltynekvojtech@gmail.com>
2024-05-25 00:33:50 +0200

    pridani teeth site v react

--- frontend/react-hack-s2t/src/main.jsx
+++ frontend/react-hack-s2t/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Welcome from './welcome';
+import Teeth from './teeth';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/app" element={<App />} />
+        <Route path="/teeth" element={<Teeth />} />
       </Routes>
     </Router>
   </React.StrictMode>
